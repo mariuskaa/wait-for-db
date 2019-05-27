610394cb85f11cf4b004aea080be7bf3a098e8ac
Marius Kaasbøll <marius.kaasboll@gmail.com>
2019-05-27 23:27:12 +0200

    Added timeout parameter

--- main.go
+++ main.go
@@ -7,37 +7,53 @@ import (
 	"time"
 	"os/exec"
 	"os"
+	"flag"
 )
 
 func main() {
+	const (
+		defaultTimeout = 5
+		timeoutDescription = "seconds between retries"
+	)
 
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s <connection> <command>\n", os.Args[0])
+	var count int
+	flag.IntVar(&count, "timeout", defaultTimeout, timeoutDescription)
+	flag.IntVar(&count, "t", defaultTimeout, timeoutDescription)
+	flag.Parse()
+
+
+	if len(flag.Args()) < 2 {
+		fmt.Printf("Usage: %s [options] <connection> <command>\n", os.Args[0])
 		os.Exit(3)
 	}
 
-	fmt.Println("waiting for service to come online");
-	db, err := sql.Open("mysql", os.Args[1])
+	var (
+		connection = flag.Args()[0]
+		command = flag.Args()[1]
+	)
+
+	fmt.Println("waiting for database to come online");
+
+	db, err := sql.Open("mysql", connection)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for {
 		err = db.Ping()
-		if err != nil {
-			fmt.Println("attempting to restart...")
-			time.Sleep(10 * time.Second)
-		} else {
+		if err == nil {
 			fmt.Println("service is ready to go!")
-			cmd := exec.Command(os.Args[2])
+			cmd := exec.Command(command)
 			output, err := cmd.CombinedOutput()
 			if err != nil {
-				fmt.Println(fmt.Sprint(err) + ": " + string(output));
-				log.Fatal("ERROR")
+				log.Fatal(fmt.Sprint(err) + ":\n    " + string(output));
 			}else{
-				log.Printf("%s\n", output)
+				log.Printf("out: %s\n", output)
 			}
 			break;
+		} else {
+			fmt.Println("attempting to restart...")
+			time.Sleep(time.Duration(count) * time.Second)
 		}
 	}
 }
